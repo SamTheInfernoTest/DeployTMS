e8507b3cf6433df82e0b8e5cd29aa43d67ccb90f
saurabh <sumsau2022@gmail.com>
2024-12-01 21:29:00 +0530

    doing work on settings

--- tmsfrontend/src/components/settings/Settings.jsx
+++ tmsfrontend/src/components/settings/Settings.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 function Settings() {
+
+  const [tab, setTab] = useState('account')
+
+  // useEffect(() => {
+  //   document.getElementById(tab).classList.add('border-b')
+  // }, [tab])
+
   return (
-    <div>Settings</div>
+    <div>
+      <div>
+        <header className = 'p-8 '>
+          <nav>
+            <ul className= 'flex flex-row '>
+              <li id='account' className = ''>
+                <button className={`w-24 py-2 px-4 dark:border-white border-black hover:border-b ${tab === 'account' ? 'border-b font-semibold' : ''}`}
+                onClick={() => {
+                  setTab('account')
+                }}
+                >
+                  Account
+                </button>
+              </li>
+              <li id='general' className=''>
+                <button className={`w-24 py-2 px-4 dark:border-white border-black hover:border-b ${tab === 'general' ? 'border-b font-semibold' : ''}`}
+                  onClick={() => {
+                    setTab('general')
+                  }}
+                >
+                  General
+                </button>
+              </li>
+            </ul>
+          </nav>
+          <hr className='dark:brightness-[25%] brightness-[90%]'/>
+        </header>
+      </div>
+    </div>
   )
 }
 
